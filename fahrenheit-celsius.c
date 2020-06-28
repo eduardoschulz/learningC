1ac62306fed7f67843f21a5a2c8246a4614bf8e9
eduardoschulz <eduardo.schulz@protonmail.com>
2020-06-28 14:34:01 -0300

    using float istead of int

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -2,8 +2,8 @@
 
 int main(){
 
-int fahr, celsius;
-int lower, upper, step;
+
+float fahr, celsius, lower, upper, step;
 
 lower = 0;
 upper = 300;
@@ -12,13 +12,13 @@ step = 20;
 fahr = lower;
 
 
-
+printf("Fahrenheit to Celcius Convertor\n");
 for (fahr = lower; fahr <= upper; fahr += step){
 
 
 
-celsius = (fahr-32) *5 /9;
-printf("%d\t%d\n", fahr, celsius);
+celsius = (fahr-32) * (5.0/9.0);
+printf("\t%3.0f°F\t%6.1f°C\n", fahr, celsius);
 
 //pagina 15 livro the c programming language
 }
