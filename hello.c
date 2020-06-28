b9cdc06f2c38224a05274a966d5abb196b97166e
eduardoschulz <eduardoschulz@github.com>
2020-06-27 23:16:48 -0300

    adionando opcoes de print

--- hello.c
+++ hello.c
@@ -2,7 +2,12 @@
 
 int main(){
 
-printf("Hello World! \n"); // \n pula linha
+printf("Hello World! \n"); // \n pula linha \t para um tab \b backspace \" para aspas duplas \\ para barra invertida
+
+printf("Texto \t");
+printf("Texto\b  ");
+printf("\"Texto\" ");
+printf("Texto \\ ");
 return 0;
 
 }
