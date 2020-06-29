4aa725fea2afb8f3caf1ea8f74653aebf62de45f
eduardoschulz <eduardo.schulz@protonmail.com>
2020-06-29 12:36:40 -0300

    removing comment

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -12,7 +12,7 @@ step = 20;
 fahr = lower;
 
 
-printf("Fahrenheit to Celcius Convertor\n");
+printf("Fahrenheit to Celcius Converter\n");
 for (fahr = lower; fahr <= upper; fahr += step){
 
 
@@ -20,7 +20,7 @@ for (fahr = lower; fahr <= upper; fahr += step){
 celsius = (fahr-32) * (5.0/9.0);
 printf("\t%3.0f°F\t%6.1f°C\n", fahr, celsius);
 
-//pagina 15 livro the c programming language
+
 }
 
 }
